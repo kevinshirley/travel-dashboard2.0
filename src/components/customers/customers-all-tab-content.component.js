497b6a009ca71cd5aa72648a62dddd53ce69aea1
Kevin Shirley <kevinxshirley@gmail.com>
2020-12-08 20:36:11 -0500

    Customer card style

--- src/components/customers/customers-all-tab-content.component.js
+++ src/components/customers/customers-all-tab-content.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as R from 'ramda';
 import cx from 'classnames';
 import Avatar from 'src/components/material-ui/avatar';
 import Badge from '@material-ui/core/Badge';
@@ -6,6 +7,9 @@ import { MORE_VERT_ICON } from 'src/components/material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
 
 function CustomersAllTabContent({ customers = [] }) {
+  const onCustomerClicked = (firstName) => {
+    console.log(`clicked on ${firstName} profile`);
+  };
   return (
     <section className='c-customers-all-tab-content__container'>
       {customers.map((customer, i) => {
@@ -15,27 +19,38 @@ function CustomersAllTabContent({ customers = [] }) {
         });
 
         return (
-          <div className='c-customers-all-tab-content__single' index={i}>
-            <div className='c-customers-all-tab-content__single-more'>
-              <IconButton aria-label="messages" color="inherit">{MORE_VERT_ICON}</IconButton>
+          <div className='c-customers-all-tab-content__single-wrapper'>
+            <div className='c-customers-all-tab-content__single' index={i} onClick={() => onCustomerClicked(customer.firstName)}>
+              <div className='c-customers-all-tab-content__single-more'>
+                <IconButton aria-label="messages" color="inherit">{MORE_VERT_ICON}</IconButton>
+              </div>
+              <div className='c-customers-all-tab-content__single-avatar-wrapper'>
+                <Badge
+                  badgeContent=" "
+                  className={badgeClasses}
+                  color="secondary"
+                  overlap="circle"
+                  variant="dot"
+                >
+                  <Avatar>
+                    {customer.profileImage}
+                  </Avatar>
+                </Badge>
+              </div>
+              <div className='c-customers-all-tab-content__single-details'>
+                <span className='name'>{`${customer.firstName} ${customer.lastName}`}</span>
+                <span className='email'>{customer.email}</span>
+                <span className='phoneNumber'>{customer.phoneNumber}</span>
+              </div>
             </div>
-            <div className='c-customers-all-tab-content__single-avatar-wrapper'>
-              <Badge
-                badgeContent=" "
-                className={badgeClasses}
-                color="secondary"
-                overlap="circle"
-                variant="dot"
-              >
-                <Avatar>
-                  {customer.profileImage}
-                </Avatar>
-              </Badge>
+            <div className='c-customers-all-tab-content__single-buttons'>
+              <div className='c-customers-all-tab-content__single-button'>
+                <span>{R.toUpper('Trips')}</span>
+              </div>
+              <div className='c-customers-all-tab-content__single-button'>
+                <span>{R.toUpper('Notes')}</span>
+              </div>
             </div>
-            {customer.firstName}
-            {customer.lastName}
-            {customer.email}
-            {customer.phoneNumber}
           </div>
         );
       })}
