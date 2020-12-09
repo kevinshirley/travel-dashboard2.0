c307f098908691197fed3d242bd2f9830ab679c7
Kevin Shirley <kevinxshirley@gmail.com>
2020-12-08 23:09:49 -0500

    Customer card layout

--- src/components/customers/customers-all-tab-content.component.js
+++ src/components/customers/customers-all-tab-content.component.js
@@ -12,48 +12,50 @@ function CustomersAllTabContent({ customers = [] }) {
   };
   return (
     <section className='c-customers-all-tab-content__container'>
-      {customers.map((customer, i) => {
-        const badgeClasses = cx('c-customers-all-tab-content__single-avatar', {
-          'badge__is-online': customer.isOnline,
-          'badge__not-online': !customer.isOnline,
-        });
+      <div className='inner'>
+        {customers.map((customer, i) => {
+          const badgeClasses = cx('c-customers-all-tab-content__single-avatar', {
+            'badge__is-online': customer.isOnline,
+            'badge__not-online': !customer.isOnline,
+          });
 
-        return (
-          <div className='c-customers-all-tab-content__single-wrapper'>
-            <div className='c-customers-all-tab-content__single' index={i} onClick={() => onCustomerClicked(customer.firstName)}>
-              <div className='c-customers-all-tab-content__single-more'>
-                <IconButton aria-label="messages" color="inherit">{MORE_VERT_ICON}</IconButton>
+          return (
+            <div className='c-customers-all-tab-content__single-wrapper'>
+              <div className='c-customers-all-tab-content__single' index={i} onClick={() => onCustomerClicked(customer.firstName)}>
+                <div className='c-customers-all-tab-content__single-more'>
+                  <IconButton aria-label="messages" color="inherit">{MORE_VERT_ICON}</IconButton>
+                </div>
+                <div className='c-customers-all-tab-content__single-avatar-wrapper'>
+                  <Badge
+                    badgeContent=" "
+                    className={badgeClasses}
+                    color="secondary"
+                    overlap="circle"
+                    variant="dot"
+                  >
+                    <Avatar>
+                      {customer.profileImage}
+                    </Avatar>
+                  </Badge>
+                </div>
+                <div className='c-customers-all-tab-content__single-details'>
+                  <span className='name'>{`${customer.firstName} ${customer.lastName}`}</span>
+                  <span className='email'>{customer.email}</span>
+                  <span className='phoneNumber'>{customer.phoneNumber}</span>
+                </div>
               </div>
-              <div className='c-customers-all-tab-content__single-avatar-wrapper'>
-                <Badge
-                  badgeContent=" "
-                  className={badgeClasses}
-                  color="secondary"
-                  overlap="circle"
-                  variant="dot"
-                >
-                  <Avatar>
-                    {customer.profileImage}
-                  </Avatar>
-                </Badge>
-              </div>
-              <div className='c-customers-all-tab-content__single-details'>
-                <span className='name'>{`${customer.firstName} ${customer.lastName}`}</span>
-                <span className='email'>{customer.email}</span>
-                <span className='phoneNumber'>{customer.phoneNumber}</span>
+              <div className='c-customers-all-tab-content__single-buttons'>
+                <div className='c-customers-all-tab-content__single-button'>
+                  <span>{R.toUpper('Trips')}</span>
+                </div>
+                <div className='c-customers-all-tab-content__single-button'>
+                  <span>{R.toUpper('Notes')}</span>
+                </div>
               </div>
             </div>
-            <div className='c-customers-all-tab-content__single-buttons'>
-              <div className='c-customers-all-tab-content__single-button'>
-                <span>{R.toUpper('Trips')}</span>
-              </div>
-              <div className='c-customers-all-tab-content__single-button'>
-                <span>{R.toUpper('Notes')}</span>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </section>
   );
 }
