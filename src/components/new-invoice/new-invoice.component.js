3ea9195296a4b0963b61cb60fd7c00b5ab81ec89
Kevin Shirley <kevinxshirley@gmail.com>
2020-12-29 10:56:30 -0500

    New invoice company + client details

--- src/components/new-invoice/new-invoice.component.js
+++ src/components/new-invoice/new-invoice.component.js
@@ -47,8 +47,34 @@ function NewInvoice() {
             <span className={`${BEM_BLOCK}__country`}>United States</span>
           </div>
           <div className={`${BEM_BLOCK}__billed-to-date`}>
-            <div></div>
+            <div className={`${BEM_BLOCK}__date-issued`}>
+              <span className={`${BEM_BLOCK}__date-issued-title`}>Date Issued</span>
+              <span className={`${BEM_BLOCK}__date`}>12/28/2020</span>
+            </div>
+            <div className={`${BEM_BLOCK}__due-date`}>
+              <span className={`${BEM_BLOCK}__due-date-title`}>Due Date</span>
+              <span className={`${BEM_BLOCK}__date`}>01/12/2021</span>
+            </div>
           </div>
+          <div className={`${BEM_BLOCK}__invoice-id`}>
+            <div className={`${BEM_BLOCK}__invoice-number`}>
+              <span className={`${BEM_BLOCK}__invoice-number-title`}>Invoice Number</span>
+              <span className={`${BEM_BLOCK}__number`}>1000011111222000</span>
+            </div>
+            <div className={`${BEM_BLOCK}__reference-number`}>
+              <span className={`${BEM_BLOCK}__reference-number-title`}>Reference</span>
+              <span className={`${BEM_BLOCK}__number`}>363528</span>
+            </div>
+          </div>
+          <div className={`${BEM_BLOCK}__amount-due`}>
+            <span className={`${BEM_BLOCK}__amount-due-title`}>Amount Due (USD)</span>
+            <span className={`${BEM_BLOCK}__amount`}>$2232.99</span>
+          </div>
+        </div>
+        <div className={`${BEM_BLOCK}__invoice-message`}>
+          <span className={`${BEM_BLOCK}__message`}>
+            This is for your trip itinerary to Venezuela. Thanks for your business and please contact for more details.
+          </span>
         </div>
       </div>
     </div>
