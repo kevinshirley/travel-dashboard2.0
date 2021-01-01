1ede825c178767f02b0f19652ae75e086331890d
Kevin Shirley <kevinxshirley@gmail.com>
2020-12-31 21:29:37 -0500

    Able to toggle item name edit input

--- src/components/new-invoice/invoice-breakdown-line.component.js
+++ src/components/new-invoice/invoice-breakdown-line.component.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 const BEM_BLOCK = 'c-new-invoice';
 
@@ -10,11 +11,49 @@ function InvoiceBreakdownLine({
   total,
   unitCost,
 }) {
+  const itemNameState = {'item-name-state': 'display'};
+  const editItemNameState = {'item-name-state': 'edit'};
+  const [shouldEditItemName, setShouldEditItemName] = useState(false);
+
+  const onToggleItemNameState = e => {
+    const target = e.target.getAttribute('item-name-state');
+
+    if (target === 'display') {
+      setShouldEditItemName(true);
+    } else if (target === 'edit') {
+      setShouldEditItemName(false);
+    }
+  };
+
+  const itemNameClasses = cx(`${BEM_BLOCK}__item--title`, {
+    [`${BEM_BLOCK}__item--title--hidden`]: shouldEditItemName,
+  });
+
+  const editItemNameClasses = cx(`${BEM_BLOCK}__item--edit-title`, {
+    [`${BEM_BLOCK}__item--edit-title--hidden`]: !shouldEditItemName,
+  });
+
   return (
     <div className={`${BEM_BLOCK}__row--line`}>
       <div className={`${BEM_BLOCK}__description--item`}>
-        <span className={`${BEM_BLOCK}__item--title`}>{itemName ? itemName : 'Enter an item name'}</span>
-        <span className={`${BEM_BLOCK}__item--sub-title`}>{itemdescription ? itemdescription : 'Enter an item description'}</span>
+        <span
+          className={itemNameClasses}
+          onClick={e => onToggleItemNameState(e)}
+          {...itemNameState}
+        >
+          {itemName ? itemName : 'Enter an item name'}
+        </span>
+        <input
+          className={editItemNameClasses}
+          type='text'
+          name='itemName'
+          placeholder='Enter an item name'
+          onBlur={e => onToggleItemNameState(e)}
+          {...editItemNameState}
+        />
+        <span className={`${BEM_BLOCK}__item--sub-title`}>
+          {itemdescription ? itemdescription : 'Enter an item description'}
+        </span>
       </div>
       <div className={`${BEM_BLOCK}__totals--items`}>
         <div className={`${BEM_BLOCK}__unit-cost-item--content`}>
