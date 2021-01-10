3412c67fa78a049d112c72677c65bb8f9b4f35ad
Kevin Shirley <kevinxshirley@gmail.com>
2021-01-10 14:00:21 -0500

    Commented amplify login

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -9,27 +9,28 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function Home() {
   const [user, setUser] = useState(null)
-  useEffect(() => {
-    // Access the user session on the client
-    Auth.currentAuthenticatedUser()
-      .then(user => {
-        console.log("User: ", user)
-        setUser(user)
-      })
-      .catch(err => setUser(null))
-  }, [])
+  // useEffect(() => {
+  //   // Access the user session on the client
+  //   Auth.currentAuthenticatedUser()
+  //     .then(user => {
+  //       console.log("User: ", user)
+  //       setUser(user)
+  //     })
+  //     .catch(err => setUser(null))
+  // }, [])
 
   return (
     <>
       <PageHeader title='Travel Dashboard' />
       <HomeSection1 />
       <HomeSection2 />
-      <div className='c-generic-modal-form'>
+      {/* <div className='c-generic-modal-form'>
         { user && <h1>Welcome, {user.attributes.email}</h1> }
         <AmplifySignOut />
-      </div>
+      </div> */}
     </>
   );
 };
 
-export default withAuthenticator(Home);
+// export default withAuthenticator(Home);
+export default Home;
