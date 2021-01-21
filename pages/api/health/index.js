b9242a9adc56b7ae2fac5099e884cf1d3399efcc
Kevin Shirley <kevinxshirley@gmail.com>
2021-01-20 20:27:02 -0500

    debug env vars

--- pages/api/health/index.js
+++ pages/api/health/index.js
@@ -1,11 +1,10 @@
 const Health = (req, res) => {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
   return res.send({
     success: true,
     message: 'This API is healty!',
     appUrl: process.env.APP_URL,
     firebaseProjectKey: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    firebasePrivateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+    firebasePrivateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     firebaseClientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     firebaseDatabaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
